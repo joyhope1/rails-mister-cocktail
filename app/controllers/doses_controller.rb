925340f64f35d2f683615c18f8a1dbde1e093435
Andrea Hostetter <alhostetter@gmail.com>
2018-10-25 17:24:40 -0500

    7 failures to complete

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,2 +1,21 @@
 class DosesController < ApplicationController
+  def new
+    @dose = Dose.new(params[:cocktail_id])
+  end
+
+  def create
+    @dose = Dose.create!(dose_params)
+      redirect_to new_dose_path(@cocktail)
+  end
+
+  def destroy
+    @doses = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+private
+
+  def dose_params
+    params.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
+  end
 end
